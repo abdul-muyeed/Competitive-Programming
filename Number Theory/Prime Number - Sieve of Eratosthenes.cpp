d8ff9e3c3010c91ffe01891e4f2bfefcc434e39b
Abdul Muyeed <102542790+AbdulMuyeed47@users.noreply.github.com>
2022-07-05 11:56:50 +0600

    Added Prime Number - Sieve of Eratosthenes
    This code can print prime numbers from 0 to n. This is used from prime number related problems.

---
+++ Number Theory/Prime Number - Sieve of Eratosthenes.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long int N=1e6;
+
+
+// This a code of Sieve of Eratosthenes
+
+
+int main()
+{
+	vector<bool> v(N,1);
+	v[0]=v[1]=0;
+	for(long long int i=2;i*i<=N;i++ ){
+		if(v[i]){
+			for(long long int j=i;j<=N/i;j++){
+				v[i*j]=0;
+			}
+		}
+	}
+	long long int n=100; //Enter last number
+
+	for(long long int i=1;i<=n;i++){
+		if(v[i]){
+			cout<<i<<endl; // prints prime numbers from 2 to n
+		}
+	}
+
+
+	return 0;
+}
