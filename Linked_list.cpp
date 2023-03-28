a2114dac8eb0bb0e41e3e0f73c488a7892040a0b
Abdul Muyeed <abdulmuyeed47@gmail.com>
2023-03-29 04:41:54 +0600

    linked list

--- Linked_list.cpp
+++ Linked_list.cpp
@@ -1,21 +1,51 @@
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-struct Node
-{
+class node{
+	public:
 	int data;
-	struct Node *next;
+	node* next;
+	node(int val){
+		data = val;
+		next = NULL;
+	}
 };
-
-void push(Node** head_ref, int new_data) {
-	Node* new_node = new Node();
-	new_node->data = new_data;
-	new_node->next = (*head_ref);
-	(*head_ref) = new_node;
+void insertAtStart(node* &head, int val){
+	node* n = new node(val);
+	n->next = head;
+	head = n;
 }
-int main()
-{
-	
+void insertAtEnd(node* &head, int val){
+	node* n = new node(val);
+	if(head==NULL){
+		head = n;
+		return;
+	}
+	node* temp = head;
+	while(temp->next != NULL){
+		temp = temp->next;
+	}
+	temp->next = n;
+}
+void display(node* head){
+	node* temp = head;
+	while(temp != NULL){
+		cout<<temp->data<<" ";
+		temp = temp->next;
+	} cout<<"\n";
+}
+
+int main(){
+	node* head = NULL;
+	int n;
+	cin>> n;
+	for(int i=0; i<n; i++){
+		int num;
+		cin>> num;
+		insertAtEnd(head, num);
+	}
+	display(head);
+	insertAtStart(head,1000);
+	display(head);
 	return 0;
 }
-
